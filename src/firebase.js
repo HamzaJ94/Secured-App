4e6429e003d6e93d46e01ae7cdf27954a17bb98a
Hamza J <104732125+HJ1111111@users.noreply.github.com>
2024-08-21 12:07:33 +0200

    firebaseconfig hidden dotenv

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDdGBctJkF7nKBZVFmWTZ0YuxCvJGojcRg",
-  authDomain: "react-app-9b63e.firebaseapp.com",
-  projectId: "react-app-9b63e",
-  storageBucket: "react-app-9b63e.appspot.com",
-  messagingSenderId: "354226394108",
-  appId: "1:354226394108:web:da44c6c4d915e1d2861879",
-  measurementId: "G-6E4YM4N4ME"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
 // Initialize Firebase
